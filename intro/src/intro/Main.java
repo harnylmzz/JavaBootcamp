e30516ddb4bc8e985be85d6a6310581f17f00165
Harun <Harun@DESKTOP-TUUM8DC>
2022-09-21 18:17:02 +0300

    variables

--- intro/src/intro/Main.java
+++ intro/src/intro/Main.java
@@ -6,6 +6,43 @@ public class Main {
 		
         System.out.println("Hello World!");
         
+        String ortaMetin = "İlginizi çekebilir :)";
+        String altMetin = "Vade süresi";		
+        
+        System.out.println(ortaMetin);
+        System.out.println(altMetin);
+        
+        int vade = 12; 
+        
+        double dolarDun = 18.25;
+        double dolarBugun = 18.35;
+        
+        boolean dolarDustuMu = false;
+        
+        String okYonu = "";
+        
+        if (dolarBugun<dolarDun) {
+        	okYonu = "down.svg";
+			System.out.println(okYonu);
+		} 
+        else if (dolarBugun>dolarDun) {
+        	
+        	
+        	okYonu = "up.svg";
+			System.out.println(okYonu);
+		}
+        else {
+        	okYonu = "equal.svg";
+			System.out.println(okYonu);       	
+        }
+        
+        //array
+        
+        String [] krediler = {"Hızlı kredi", "Maaşını Halkbank'tan Alankar", "Emekliler", "Emekli Olmak İsteyenler"};
+      
+        for (int i = 0; i < krediler.length; i++) {
+            System.out.println(krediler[i]);
+        
 	}
-
+	}
 }
