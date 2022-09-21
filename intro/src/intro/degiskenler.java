60870a80c0eda807dffa1f2315a0f8e3633ce127
Harun <Harun@DESKTOP-TUUM8DC>
2022-09-21 18:10:51 +0300

    variables

---
+++ intro/src/intro/degiskenler.java
@@ -0,0 +1,12 @@
+package intro;
+
+public class degiskenler {
+	
+	public static void main(String[] args) {
+		
+		int numberOfStudent = 32; 
+		String sayı = "Öğrenci sayısı";  
+		System.out.println( sayı +numberOfStudent);
+	}
+
+}
