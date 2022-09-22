15d2741bad260c113b391d49d8299b9706271253
Harun <Harun@DESKTOP-TUUM8DC>
2022-09-22 17:39:57 +0300

    Şart blokları ödevi tamamlandı.

---
+++ intro/src/intro/conditionals.java
@@ -0,0 +1,26 @@
+package intro;
+
+public class conditionals {
+
+	public static void main(String[] args) {
+		
+	int clock = 16;
+	boolean saatGeciMi = false; 
+	
+	if(clock<19) {
+		
+		
+		System.out.println("saat 18'i gecmedi");
+	}
+	else if (clock==16) { 
+		System.out.println("saat 16");
+	}
+
+	else { 
+		System.out.println("saat 18'i gecti");
+		
+		
+	}
+
+	}
+}
