7bc2a5cb05616f4d272b7d76c9c0bda58cf4bd55
Harun <Harun@DESKTOP-TUUM8DC>
2022-09-28 12:29:37 +0300

    Methods2 ödevi tamamlandı

---
+++ HomeWorks/src/Methods2.java
@@ -0,0 +1,34 @@
+
+public class Methods2 {
+	public static void main(String[] args) {
+		int  sayi = topla (5,7);
+		System.out.println(sayi);
+
+
+	}
+
+	public static void ekle() {
+		System.out.println("Eklendi.");
+
+	}
+
+	public static void sil() {
+		System.out.println("Silindi.");
+
+
+	}
+
+	public static void güncelle() {
+		System.out.println("Güncellendi.");
+
+
+	}
+	
+	public static int topla (int sayi1, int sayi2 ) {
+		return sayi1+sayi2;
+		
+	}
+
+	
+
+}
