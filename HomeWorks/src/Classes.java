1ff22e0049b60691505ece733e8c7c4f9072ab3f
Harun <Harun@DESKTOP-TUUM8DC>
2022-10-01 18:53:30 +0300

    Ödev video 27-28 tamamlandı

---
+++ HomeWorks/src/Classes.java
@@ -0,0 +1,13 @@
+
+public class Classes {
+	public static void main(String[] args) {
+		
+		CustomerManager customerManager = new CustomerManager();
+		//classlar referans tiptir = Reference type 
+		customerManager.Add();
+		customerManager.Remove();
+		customerManager.Update();
+		
+	}
+	
+}
