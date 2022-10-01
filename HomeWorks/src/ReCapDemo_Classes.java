002399b7020fd268f9681745a1e59a357736caaf
Harun <Harun@DESKTOP-TUUM8DC>
2022-10-01 21:45:30 +0300

    ReCapDemo_Classes & Dortislem tamamlandı

---
+++ HomeWorks/src/ReCapDemo_Classes.java
@@ -0,0 +1,10 @@
+
+public class ReCapDemo_Classes {
+	public static void main(String[] args) {
+		
+		Dortislem dortislem = new Dortislem();
+		int sonuc = dortislem.Topla(8, 4);
+		System.out.println(sonuc);
+	}
+
+}
