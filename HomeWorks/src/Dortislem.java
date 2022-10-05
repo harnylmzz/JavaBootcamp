9034fa761859d6482b00288f4c1b2ea58132e794
Harun <Harun@DESKTOP-TUUM8DC>
2022-10-05 11:16:14 +0300

    Overloading

--- HomeWorks/src/Dortislem.java
+++ HomeWorks/src/Dortislem.java
@@ -1,17 +1,12 @@
 
 public class Dortislem {
-	public int Topla (int sayi1, int sayi2) {
-		return sayi1+sayi2;
-	}
-	public int Cikar (int sayi1, int sayi2) {
-		return sayi1-sayi2;
+	
+	public int topla (int sayi1, int sayi2) {
+		return sayi1 + sayi2;
 	}
 	
-	public int Carp (int sayi1, int sayi2) {
-		return sayi1*sayi2;
+	public int topla (int sayi1, int sayi2, int sayi3) {
+		return sayi1 + sayi2;
 	}
-	
-	public int Bol (int sayi1, int sayi2) {
-		return sayi1/sayi2;
-	}
+
 }
---
+++ HomeWorks/src/MethodOverLoading.java
@@ -0,0 +1,10 @@
+
+public class MethodOverLoading {
+	public static void main(String[] args) {
+		
+		Dortislem dortislem = new Dortislem();
+		System.out.println(dortislem.topla(1, 3));
+		dortislem.topla(1, 3, 4);
+	}
+
+}
