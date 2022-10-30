943be559f322fe0543f58d97d359f1022a5e78b6
Harun Yılmaz <harnylmzzonline@gmail.com>
2022-10-30 16:53:44 +0300

    LessonTo4.Week

---
+++ rentACar/src/main/java/kodlama/io/rentACar/buisness/concretes/BrandManager.java
@@ -0,0 +1,27 @@
+package kodlama.io.rentACar.buisness.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlama.io.rentACar.buisness.abstracts.BrandService;
+import kodlama.io.rentACar.dataAccess.Abstracts.BrandRepository;
+import kodlama.io.rentACar.entities.concretes.Brand;
+
+@Service  // bu sınıf bir buisness nesnesidir
+public class BrandManager implements BrandService {
+	private BrandRepository brandRepository;
+
+	@Autowired
+	public BrandManager(BrandRepository brandRepository) {
+		this.brandRepository = brandRepository;
+	}
+
+	@Override
+	public List<Brand> getAll() {
+		// iş kuralları
+		return brandRepository.getAll();
+	}
+
+}
